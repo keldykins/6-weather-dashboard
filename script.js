66b2354dab99ad77142d955fd46397a296b55108
keldykins <kelcundy@gmail.com>
2020-10-19 15:03:03 -0600

    saving work

--- script.js
+++ script.js
@@ -2,19 +2,15 @@
 // when i click the search button my primary card is populated with the specified city and its current weather data
 // underneath the primary card are 5 badges that display the estimated weather data for the next 5 days
 // underneath the search field is a vertical unordered list with the last 8 previously searched cities being dynamically appended
-
 // * pulls information from the form and builds the query URL
 //  * @returns {string} URL for OWM API based on form inputs
 //  */
-
 function getForecast(city) {
   // queryURL is the url we'll use to query the API
   var queryURL = "api.openweathermap.org/data/2.5/forecast?q=";
-
   // Begin building an object to contain our API call's query parameters
   // Set the API key
   var apiKey = "b8d00ab246e7b992f89eedfb2df715ff";
-
   var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
   $.ajax({
     url: queryURL,
@@ -32,39 +28,30 @@ function getForecast(city) {
       // console.log(day);
       // console.log(response.list[i]);
       // console.log(response.list[i].dt_txt.split(" ")[0]);
-      if (
-        day === undefined ||
-        day !== response.list[i].dt_txt.split(" ")[0] ||
-        i === response.list.length - 1
-      ) {
+      if (day === undefined || day !== response.list[i].dt_txt.split(" ")[0]) {
         console.log("new day");
-        console.log(dailyLows);
         if (dailyLows.length > 0) {
-          averageLows.push(
-            Math.min(...dailyLows)
-            // Math.round(dailyLows.reduce((a, b) => a + b, 0) / dailyLows.length)
-          );
+          averageLows.push(Math.round(Math.min(...dailyLows)));
           dailyLows = [];
-          console.log("lows", averageLows);
         }
         if (dailyHighs.length > 0) {
-          averageHighs.push(
-            Math.round(
-              Math.max(...dailyHighs)
-              // dailyHighs.reduce((a, b) => a + b, 0) / dailyHighs.length
-            )
-          );
+          averageHighs.push(Math.round(Math.max(...dailyHighs)));
           dailyHighs = [];
-          console.log("highs", averageHighs);
         }
         day = response.list[i].dt_txt.split(" ")[0];
       } else {
         console.log("same day");
-        dailyHighs.push(response.list[i].main.temp_max);
-        dailyLows.push(response.list[i].main.temp_min);
       }
+      dailyHighs.push(response.list[i].main.temp_max);
+      dailyLows.push(response.list[i].main.temp_min);
+      if (i === response.list.length - 1) {
+        console.log("last day");
+        averageHighs.push(Math.round(Math.max(...dailyHighs)));
+        averageLows.push(Math.round(Math.min(...dailyLows)));
+      }
+      console.log("lows", averageLows);
+      console.log("highs", averageHighs);
     }
-
     // $("#city-card").text(JSON.stringify(response));
   });
 }
@@ -74,5 +61,4 @@ $("#city-search").on("submit", function (event) {
   var queryParams = $("input[id=search]").val().trim();
   getForecast(queryParams);
 });
-
 // $("input[id=name]")
